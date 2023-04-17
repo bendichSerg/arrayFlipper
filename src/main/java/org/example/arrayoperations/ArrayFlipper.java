9bc0e2a298f2dd8d42a6dda623531bed2e6be4c8
bendichsm22pv191 <serg.bendich@gmail.com>
2023-04-17 17:51:14 +0300

    changed the "flip " function to recursion

--- src/main/java/org/example/arrayoperations/ArrayFlipper.java
+++ src/main/java/org/example/arrayoperations/ArrayFlipper.java
@@ -9,16 +9,40 @@ import lombok.Setter;
 @AllArgsConstructor
 public class ArrayFlipper {
     public int[] flip(int[] array) {
-        if ((array == null) || (array.length == 0)) {
+        if (array == null || array.length == 0) {
             return new int[0];
         }
-        int len = array.length;
-        int flipLen = len / 2;
-        for (int i = 0; i < flipLen; ++i) {
-            array[i] = array[i] + array[len - i - 1];
-            array[len - i - 1] = array[i] - array[len - i - 1];
-            array[i] -= array[len - i - 1];
+        return recursionFlip(array, 0);
+    }
+
+    public int[] recursionFlip(int[] array, int changeNum) {
+        if (changeNum < array.length / 2) {
+            int tmpNum;
+            tmpNum = array[array.length - changeNum - 1];
+            array[array.length - changeNum - 1] = array[changeNum];
+            array[changeNum] = tmpNum;
+            changeNum++;
+            return recursionFlip(array, changeNum);
         }
         return array;
     }
+
 }
+
+
+//public class ArrayFlipper {
+//    static int i = 0;
+//    public int[] flip(int[] array) {
+//        if (array == null || array.length == 0) {
+//            return new int[0];
+//        }
+//        int tmpNum;
+//        for (int i = 0; i < array.length / 2 ; ++i) {
+//            tmpNum = array[array.length - i - 1];
+//            array[array.length - i - 1] = array[i];
+//            array[i] = tmpNum;
+//        }
+//        return flip(array);
+//    }
+//
+//}
